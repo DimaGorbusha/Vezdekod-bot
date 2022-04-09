febbf1f0463aec256ab4ba7fb244aa79168ec3a2
DmitriyGorbunov1 <dmitrgor1@yandex.ru>
2022-04-10 01:19:20 +0500

    FC

--- bots/db/data_base.py
+++ bots/db/data_base.py
@@ -5,31 +5,79 @@ class DB():
         self.db_con = sqlite3.connect('VK_bot.db', check_same_thread=False)
         self.cursor = self.db_con.cursor()
 
+
     def __init__(self):
         DB.__DB_connect()
         try:
-            create_table_query = '''CREATE TABLE IF NOT EXISTS vk_bot_memes (
+            create_table_query = '''CREATE TABLE IF NOT EXISTS vk_bot_users (
                                 user_id INTEGER PRIMARY KEY UNIQUE,
-                                dizlikes INTEGER,
-                                likes INTEGER);'''
+                                likes INTEGER,
+                                dizlikes INTEGER);'''
 
             self.cursor.execute(create_table_query)
             print("tb create")
             self.bd_con.commit()
             self.cursor.close() 
-
+        
         finally:
             self.db_con.close()
             print("con close")   
     
 
-    def DB_insert_data():
+    def __id_exist(self, id):
+        DB.__DB_connect()
+        try:
+            id_search_query = f"SELECT id FROM vk_bot_users WHERE ID = {id}"
+            self.cursor.execute(id_search_query)
+            res = self.cursor.fetchone()
+            return res != None
+
+        finally:
+            self.db_con.close()
+            print("id s con close")
 
 
+    def __update_data(self, user_id, likes, dizlikes):
+        DB.__DB_connect()
+        try:
+            update_query = "UPDATE vk_bot_users SET "
+        finally:
+            self.db_con.close()
+            print("upd dt con close")
 
-    def DB_update_data():
+    def search_id(self, id):
+        DB.__DB_connect()
+        try:
+            id_search_query = f"SELECT id FROM vk_bot_users WHERE ID = {id}"
+            self.cursor.execute(id_search_query)
+            res = self.cursor.fetchone()
+            print(res)
+            self.bd_con.commit()
+            self.cursor.close() 
+            return res
+        
+        finally:
+            self.db_con.close()
+            print("id s con close")  
 
 
+    
 
-    def DB_id_search():
-            DB.__DB_connect()
+    def insert_data(self, user_id, likes, dizlikes):
+        DB.__DB_connect()
+        try:
+            if DB.__id_exist():
+                insert_query = "INSERT INTO vk_bot_users (user_id, likes, dizlikes) VALUES (%s, %s, %s)"
+                self.cursor.execute(insert_query, user_id, likes, dizlikes)
+                self.bd_con.commit()
+                self.cursor.close()
+                print("dt ins ok")
+            else:
+                DB.__update_data(user_id, likes, dizlikes)
+        
+        finally:
+            self.db_con.close()
+            print("dt ins con close") 
+
+
+    
