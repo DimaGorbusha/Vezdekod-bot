37209cc6a38aeb85adb25e2a31a3ef8c954e1d0c
DmitriyGorbunov1 <dmitrgor1@yandex.ru>
2022-04-10 01:49:18 +0500

    FC

--- bots/db/data_base.py
+++ bots/db/data_base.py
@@ -7,7 +7,8 @@ class DB():
 
 
     def __init__(self):
-        DB.__DB_connect()
+        self.__DB_connect()
+        print("всё ок")
         try:
             create_table_query = '''CREATE TABLE IF NOT EXISTS vk_bot_users (
                                 user_id INTEGER PRIMARY KEY UNIQUE,
@@ -16,7 +17,7 @@ class DB():
 
             self.cursor.execute(create_table_query)
             print("tb create")
-            self.bd_con.commit()
+            self.db_con.commit()
             self.cursor.close() 
         
         finally:
@@ -25,9 +26,9 @@ class DB():
     
 
     def __id_exist(self, id):
-        DB.__DB_connect()
+        self.__DB_connect()
         try:
-            id_search_query = f"SELECT id FROM vk_bot_users WHERE ID = {id}"
+            id_search_query = f"SELECT user_id FROM vk_bot_users WHERE user_id = {id}"
             self.cursor.execute(id_search_query)
             res = self.cursor.fetchone()
             return res != None
@@ -38,28 +39,29 @@ class DB():
 
 
     def __update_data(self, user_id, likes, dizlikes):
-        DB.__DB_connect()
+        self.__DB_connect()
         try:
-            update_query1 = f"UPDATE vk_bot_users SET likes = {likes} WHERE id = {user_id}"
-            update_query2 = f"UPDATE vk_bot_users SET dizlikes = {dizlikes} WHERE id = {user_id}"
+            update_query1 = f"UPDATE vk_bot_users SET likes = {likes} WHERE user_id = {user_id}"
+            update_query2 = f"UPDATE vk_bot_users SET dizlikes = {dizlikes} WHERE user_id = {user_id}"
             self.cursor.execute(update_query1)
             self.cursor.execute(update_query2)
             print("dt upt")
-            self.bd_con.commit()
+            self.db_con.commit()
             self.cursor.close()
 
         finally:
             self.db_con.close()
             print("upd dt con close")
 
+
     def search_id(self, id):
-        DB.__DB_connect()
+        self.__DB_connect()
         try:
-            id_search_query = f"SELECT id FROM vk_bot_users WHERE ID = {id}"
+            id_search_query = f"SELECT user_id FROM vk_bot_users WHERE user_id = {id}"
             self.cursor.execute(id_search_query)
             res = self.cursor.fetchone()
             print(res)
-            self.bd_con.commit()
+            self.db_con.commit()
             self.cursor.close() 
             return res
         
@@ -69,16 +71,16 @@ class DB():
 
 
     def insert_data(self, user_id, likes, dizlikes):
-        DB.__DB_connect()
+        self.__DB_connect()
         try:
-            if DB.__id_exist():
+            if self.__id_exist(user_id):
                 insert_query = "INSERT INTO vk_bot_users (user_id, likes, dizlikes) VALUES (%s, %s, %s)"
                 self.cursor.execute(insert_query, user_id, likes, dizlikes)
-                self.bd_con.commit()
+                self.db_con.commit()
                 self.cursor.close()
                 print("dt ins ok")
             else:
-                DB.__update_data(user_id, likes, dizlikes)
+                self.__update_data(user_id, likes, dizlikes)
         
         finally:
             self.db_con.close()
